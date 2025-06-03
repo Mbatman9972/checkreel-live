88f5c9c244f580ec6633fa77a02a483f6d223822
Your Name <your.email@example.com>
2025-06-03 05:24:21 +0300

    Separate admin config with service_role key

---
+++ admin-config.js
@@ -0,0 +1,2 @@
+const ADMIN_SUPABASE_URL = "https://jyfpqxowaknjuhfmzofk.supabase.co";
+const ADMIN_SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5ZnBxeG93YWtuanVoZm16b2ZrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODEyNjQxNCwiZXhwIjoyMDYzNzAyNDE0fQ.AyN1-GoluVFcq59Khpvhay_vPcu1DNC6eG_b1eEFQRs";
